34b95331e068849fe1a1e0e609ad502126b9f7d0
Scott Wilson <hey@scottwil.com>
2020-10-15 13:49:06 -0700

    ios

--- src/components/BuyButton.js
+++ src/components/BuyButton.js
@@ -15,7 +15,6 @@ const BuyButton = () => {
 }
 
 const CourseButton = styled(Button)`
-  height: 100%;
   width: 75%;
   background-color: rgba(216, 0, 35, 0.7);
   border: 1px solid rgba(0, 0, 0, 0.2);
